22040d6a9609d1c206c3406a72bb10d8ccafc011
Tom <tom.piquet@gmail.com>
2021-05-31 23:31:07 -0400

    Adds action callback to UI

--- patch/V.oscillator/voscillator.cpp
+++ patch/V.oscillator/voscillator.cpp
@@ -77,81 +77,30 @@ void Voscillator::UpdateControl()
 	}
 }
 
-// TODO refactor this !
 void Voscillator::UpdateOled()
 {
 	ui->Display();
-    // m_patch.display.Fill(false);
-
-	// // Title
-	// m_patch.display.SetCursor(0, 0);
-	// std::string str  = "V.oscillator 0.01";
-    // char*       cstr = &str[0];
-    // m_patch.display.WriteString(cstr, Font_7x10, true);
-	// m_patch.display.DrawLine(0,10, m_patch.display.Width(), 10, true);
-
-	// // Menu 
-	// m_patch.display.SetCursor(0, 12);
-	// std::string str0  = "RND FineTune";
-    // char*       cstr0 = &str0[0];
-	// FontDef* font0 = &Font_7x10;
-	// if(currentMenuItem == 0){
-	// 	font0 = &Font_7x10_flipped;
-	// 	if(m_patch.encoder.Pressed()){
-	// 		for(uint i=0 ; i<1 ; i++){
-	// 			osc[i].SetFineTune(randomGenerator.Process() * 0.5f);
-	// 		}
-	// 	}
-	// }
-    // m_patch.display.WriteString(cstr0, *font0, true);
-
-	// m_patch.display.SetCursor(0, 23);
-	// std::string str1  = "RND Pan";
-    // char*       cstr1 = &str1[0];
-	// FontDef* font1 = &Font_7x10;
-	// if(currentMenuItem == 1){
-	// 	font1 = &Font_7x10_flipped;
-	// 	if(m_patch.encoder.Pressed()){
-	// 		for(uint i=0 ; i<NB_OSC ; i++){
-	// 			osc[i].SetPan(abs(randomGenerator.Process()));
-	// 		}
-	// 	}
-	// }
-    // m_patch.display.WriteString(cstr1, *font1, true);
-
-	// m_patch.display.SetCursor(0, 34);
-	// std::string str2;
-	// // str2 = "Freq: " ;
-	// str2 = "debug:" + std::to_string(static_cast<uint32_t>((sampleRate / mainFreq) / NB_OSC));
-    // char*       cstr2 = &str2[0];
-	// FontDef* font2 = &Font_7x10;
-	// if(currentMenuItem == 2){
-	// 	font2 = &Font_7x10_flipped;
-	// 	if(m_patch.encoder.Pressed()){
-	// 		osc[0].SetAmp(0);
-	// 	}
-	// }
-    // m_patch.display.WriteString(cstr2, *font2, true);
-	
-	// m_patch.display.SetCursor(0, 45);
-	// std::string str3  = "To bootloader";
-    // char*       cstr3 = &str3[0];
-	// FontDef *font3 = &Font_7x10;
-	// if(currentMenuItem == 3){
-	// 	font3 = &Font_7x10_flipped;
-	// 	if(m_patch.encoder.Pressed()){
-	// 		daisy::System::ResetToBootloader();
-	// 	}
-	// }
-	// m_patch.display.WriteString(cstr3, *font3, true);
-
-    // m_patch.display.Update();
 }
 
 void Voscillator::CreateUI(){
 	ui = new UI(std::string("V.oscillator 0.1"), m_patch);
-	ui->CreateMenuItem(std::string("RND FineTune"));
-	ui->CreateMenuItem(std::string("RND Pan"));
-	ui->CreateMenuItem(std::string("RND Amp"));
-	ui->CreateMenuItem(std::string("To Bootloader"));
+
+	ui->CreateMenuItem(std::string("RND FineTune"), [=] {
+			for(uint i=0 ; i<1 ; i++){
+				osc[i].SetFineTune(randomGenerator.Process() * 0.5f);
+			}    
+		});
+	ui->CreateMenuItem(std::string("RND Pan"), [=] {
+			for(uint i=0 ; i<1 ; i++){
+				osc[i].SetPan(randomGenerator.Process() * 0.5f);
+			}    
+		});
+	ui->CreateMenuItem(std::string("RND Amp"), [=] {
+			for(uint i=0 ; i<1 ; i++){
+				osc[i].SetAmp(randomGenerator.Process() * 0.5f);
+			}    
+		});
+	ui->CreateMenuItem(std::string("To Bootloader"), [=] {
+			daisy::System::ResetToBootloader();
+		});
 }
