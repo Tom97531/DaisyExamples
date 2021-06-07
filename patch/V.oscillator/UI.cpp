e5bd0a390015c0e6736603f47b33cf277f2ea46d
Tom <tom.piquet@gmail.com>
2021-06-07 18:50:11 -0400

    WIP better UI

--- patch/V.oscillator/UI.cpp
+++ patch/V.oscillator/UI.cpp
@@ -5,6 +5,9 @@ UI::UI(std::string title, DaisyPatch& patch){
     Title = title;
     nbItem = 0;
     currentItem = 0;
+    oledOn = true;
+    // delay in audio clock count
+    turnOffOledDelay = TURN_OFF_OLED_DELAY * m_patch.AudioSampleRate();
 }
 
 void UI::CreateMenuItem(Item* menuItem){
@@ -12,35 +15,40 @@ void UI::CreateMenuItem(Item* menuItem){
     nbItem++;
 }
 
-void UI::Display(){
+void UI::Display(uint32_t click){
     uint8_t firstItem = 0;
     m_patch.display.Fill(false);
 
-	// Title
-	m_patch.display.SetCursor(0, 0);
-	std::string str  = Title;
-    char*       cstr = &str[0];
-    m_patch.display.WriteString(cstr, Font_7x10, true);
-	m_patch.display.DrawLine(0,10, m_patch.display.Width(), 10, true);
-
-	// Menu
-    // set y position for viewable items
-    if(currentItem >= ITEM_WINDOW_SIZE){
-        firstItem = currentItem - ITEM_WINDOW_SIZE + 1;
-        // if(firstItem > )
+    if(!oledOn){
+        m_patch.display.Update();
+        return;
     }
 
-    // display items
-    for(uint8_t i=firstItem ; i<ITEM_WINDOW_SIZE+firstItem && i<nbItem ; i++){
-	    m_patch.display.SetCursor(0, 12 + (i-firstItem)*10);
-        str = itemList[i]->name;
-        m_patch.display.WriteString(cstr, *itemList[i]->currentFont, true);
+    if(click - lastUpdate < turnOffOledDelay){
+        // Title
+        m_patch.display.SetCursor(0, 0);
+        std::string str  = Title;
+        char*       cstr = &str[0];
+        m_patch.display.WriteString(cstr, Font_7x10, true);
+        m_patch.display.DrawLine(0,10, m_patch.display.Width(), 10, true);
+
+        // Menu
+        if(currentItem >= ITEM_WINDOW_SIZE){
+            firstItem = currentItem - ITEM_WINDOW_SIZE + 1;
+        }
+
+        // display items
+        for(uint8_t i=firstItem ; i<ITEM_WINDOW_SIZE+firstItem && i<nbItem ; i++){
+            m_patch.display.SetCursor(0, 12 + (i-firstItem)*10);
+            str = itemList[i]->name;
+            m_patch.display.WriteString(cstr, *itemList[i]->currentFont, true);
+        }
     }
 
     m_patch.display.Update();
 }
 
-void UI::IncrementMenuItem(int8_t increment){
+void UI::IncrementMenuItem(int8_t increment, uint32_t click){
     // reset font
     itemList[currentItem]->currentFont = &Font_7x10;
 
@@ -54,6 +62,8 @@ void UI::IncrementMenuItem(int8_t increment){
 
     // set flipped font
     itemList[currentItem]->currentFont = &Font_7x10_flipped;
+
+    lastUpdate = click;
 }
 
 void UI::EncoderPressed(){
